48a0d603d53a8a71a02175464bbd0ffd655181f9
andre-deveau <113055933+andre-deveau@users.noreply.github.com>
2022-11-01 14:27:40 -0400

    init

--- js/script.js
+++ js/script.js
@@ -10,14 +10,24 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS20-Unit3-01-JS/sw.js", {
+    scope: "/ICS20-Unit3-01-JS/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function enterClicked() {
+  //input
+  const lengthbase = parseInt(document.getElementById("base-length").value)
+  const basewidth = paresInt(document.getElementById("base-width").value)
+  const pyramidheight = paresInt(document.getElementById("pyramid-heigt").value)
+
+  //process
+  const volume = ((baselength * widthbase * pyramidheight) / 3)
+
+  //output
+  document.getElementById("volume-of-right-rectangular-pyramid").innerHTML =
+    "The are of the trapezoid is: " + volume.toFixed(2) + "cm³"
 }
